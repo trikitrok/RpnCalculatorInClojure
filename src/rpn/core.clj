83ef6335b8ce565e24797f1d09a19cf951259159
trikitrok <trikitrok@gmail.com>
2014-07-04 01:50:11 +0200

    it adds several numbers

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -1,8 +1,30 @@
-(ns rpn.core)
+(ns rpn.core 
+  (:require [clojure.string :as str]))
 
 (defn parse-int [s]
   (Integer/parseInt (re-find #"\A-?\d+" s)))
 
+(defn parse-expression [expression]
+  (let 
+    [operators {"+" +}
+     parse-token 
+     (fn [token]
+       (let [op (get operators token)]
+         (if (nil? op)
+           (parse-int token)
+           op)))]
+    (map parse-token (clojure.string/split expression #"\s"))))
+
+(defn process-symbol [stack symbol]
+  (if (number? symbol)
+    (conj stack symbol)
+    (conj (drop-last 2 stack) (apply symbol (take-last 2 stack)))))
+
 (defn evaluate [expression]
-  (parse-int expression))
+  (nth
+    (reduce 
+      process-symbol
+      []
+      (parse-expression expression))
+    0))
 
--- test/rpn/t_core.clj
+++ test/rpn/t_core.clj
@@ -5,4 +5,8 @@
 (facts "about RPN calculator"
   (fact "a number evaluates to itself"
     (evaluate "0") => 0
-    (evaluate "1") => 1))
+    (evaluate "1") => 1)
+  
+  (fact "it adds two numbers"
+    (evaluate "0 1 +") => 1
+    (evaluate "1 2 +") => 3))
